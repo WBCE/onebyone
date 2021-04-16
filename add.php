beaff7ad7b2ac0217660aa417a9fba746ce455e3
instantflorian <info@meerwinck.com>
2021-04-16 09:35:41 +0200

    Update to 0.9.1
    fix PHP 8 issues, thanks to Bernd

--- add.php
+++ add.php
@@ -21,7 +21,7 @@ $aInsert = array(
 	'content3_code'    => '',
 	'content3_wysiwyg' => '',
 	'obo_modus'        => '',
-	'obo_dimensions'   => '',	
+	'obo_dimensions'   => '',
 );
 
 
@@ -29,7 +29,12 @@ require_once __DIR__ .'/module_settings.php';
 if($use_latest_settings){
 	// if $use_latest_settings = TRUE add modus and dimensions from latest obo section
 	$sSql = "SELECT `section_id`, `obo_modus`, `obo_dimensions` FROM `{TP}mod_onebyone` ORDER BY `section_id` DESC LIMIT 1";
-	$aLatestCfg = $database->get_array($sSql)[0];
+
+	if (!empty($database->get_array($sSql))) {
+		$aLatestCfg = $database->get_array($sSql)[0];
+	} else {
+		$aLatestCfg = [];
+	}
 	if(!empty($aLatestCfg)){
 		$aInsert['obo_modus']      = $aLatestCfg['obo_modus'];
 		$aInsert['obo_dimensions'] = $aLatestCfg['obo_dimensions'];
--- info.php
+++ info.php
@@ -14,7 +14,7 @@ if(count(get_included_files()) ==1){$z="HTTP/1.0 404 Not Found";header($z);die($
 $module_directory = 'onebyone';
 $module_name = 'onebyone';
 $module_function = 'page';
-$module_version = '0.9';
+$module_version = '0.9.1';
 $module_platform = '1.4.x';
 $module_author = 'florian, Bernd Michna';
 $module_license = 'WTFPL';
@@ -24,8 +24,8 @@ $module_description = 'This module can display some contents side by side';
 
 0.8 2020/12/28 (Stefek)
 + Added setting to disable/enable 3 blocks mode (limit confusion for low skill users)
-+ Added setting that will copy modus and dimensions from latest obo section when a new section is created (add.php) 
-+ Added setting with which you can determine what dimensions can be selected by the user 
++ Added setting that will copy modus and dimensions from latest obo section when a new section is created (add.php)
++ Added setting with which you can determine what dimensions can be selected by the user
 
 
 0.8 2020/12/15
@@ -47,7 +47,7 @@ $module_description = 'This module can display some contents side by side';
 0.3 2020/05/01
 ! Convert all files from ANSI to UTF-8
 
-0.2	2020/05/01	
+0.2	2020/05/01
 + Backend Tabs
 + module_settings.php
 + Frontend classes (section id / obo_left etc.)
